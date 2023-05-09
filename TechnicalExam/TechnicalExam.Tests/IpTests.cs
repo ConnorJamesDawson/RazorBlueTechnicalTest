2717c0613bb3cf1ef58a8b166dedc1f795bbb7a4
Connor Dawson <Connor.Dawson@hotmail.co.uk>
2023-05-09 17:50:39 +0100

    Added cidr to Task 2

---
+++ TechnicalExam/TechnicalExam.Tests/IpTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalExam;
+
+namespace TechnicalExam.Tests;
+
+public class IpTests
+{
+    [TestCase("192.168.1.141", true)]
+    [TestCase("192.168.1.305", false)]
+    public void TestIp(string Ip, bool outcome)
+    {
+        bool sut = Task2IPFiltering.FilterIP(Ip);
+
+        Assert.That(sut, Is.EqualTo(outcome));
+    }
+    [TestCase("192.168.1.141", "192.168.1.0/200", true)]
+    [TestCase("192.168.1.305", "192.168.1.0/200", false)]
+    public void TestIp(string Ip,string cidr, bool outcome)
+    {
+        bool sut = Task2IPFiltering.FilterIP(Ip, cidr);
+
+        Assert.That(sut, Is.EqualTo(outcome));
+    }
+}
---
+++ TechnicalExam/TechnicalExam/Task2IPFiltering.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TechnicalExam;
+
+public class Task2IPFiltering
+{
+    public static bool FilterIP(string ip = null, string cidr = null)
+    {
+        string exampleMinIp = "192.168.1.15"; //If working with a database these values would be gotten from said database
+        string exampleMaxIp = "192.168.1.205";
+        // exampleCIDR = "192.168.1.0/24";
+
+        List<string> ipList = ip.Split(".").ToList();
+        List<string> minIpList = exampleMinIp.Split(".").ToList();
+        List<string> maxIpList = exampleMaxIp.Split(".").ToList();
+
+        List<int> ipNumbers = ipList.ConvertAll(int.Parse);
+        List<int> minIpNumbers = minIpList.ConvertAll(int.Parse);
+        List<int> maxIpNumbers = maxIpList.ConvertAll(int.Parse);
+        if(cidr != null)
+        {
+            List<string> Cidr = cidr.Split(".").ToList();
+            List<string> range = Cidr[3].Split("/").ToList();
+            List<int> rangenumbers = range.ConvertAll(int.Parse);
+
+            if (ipNumbers[3] > rangenumbers[0] && ipNumbers[3] < rangenumbers[1]) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < ipNumbers.Count; i++)
+        {
+            if (ipNumbers[i] < minIpNumbers[i] || ipNumbers[i] > maxIpNumbers[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool FilterIps(IEnumerable<string> Ips)
+    {
+        bool result = true;
+        foreach (string ip in Ips)
+        {
+            result = FilterIP(ip);
+            if(!result) 
+                return false;
+        }
+        return true;
+    }
+
+}
